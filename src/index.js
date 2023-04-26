7b0d2cf8ee1d82319b018a307fd236917eae8030
ravi-singh <ravirainbow2003@gmail.com>
2023-04-27 05:19:51 +0530

    frontend

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 import ChatProvider from "./Context/ChatProvider";
 
-export const server = "http://localhost:5000/api";
+// export const server = "http://localhost:5000/api";
+export const server = "https://chitchatbackend.onrender.com/api";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
